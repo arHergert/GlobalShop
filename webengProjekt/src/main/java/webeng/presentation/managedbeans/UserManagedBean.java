4210cfe2bb7ce734fa68a225f2960fdea1111f0e
arHergert <hergert.artur@web.de>
2017-06-27 21:35:22 +0200

    ES FUNKTIONIERT LOL

--- webengProjekt/src/main/java/webeng/presentation/managedbeans/UserManagedBean.java
+++ webengProjekt/src/main/java/webeng/presentation/managedbeans/UserManagedBean.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
@@ -24,7 +25,8 @@ public class UserManagedBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private UserManager manager;
 	private User user;
-
+	private Map<String, Integer> warenkorb;
+	private List<String> warenkorbKeys;
 	public UserManagedBean() {
 		
 	}
@@ -35,9 +37,9 @@ public class UserManagedBean implements Serializable {
 		manager = new UserManager();
 		
 		
-		//MockUp User initialisieren
+		//MockUp Daten initialisieren
 		user = new User();
-		
+		warenkorb = new TreeMap<>();
 	}
 	
 	public User getUser() {
@@ -57,16 +59,14 @@ public class UserManagedBean implements Serializable {
 		//MockupUser anmelden und in Session speichern
 		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("loggedUser", user);
 		
-		HashMap<Product, Integer> warenkorb = new HashMap<>();
+		
 		ProductManager pm = new ProductManager();
-		warenkorb.put(pm.getProducts().get(1), 2);
-		warenkorb.put(pm.getProducts().get(2), 3);
-		warenkorb.put(pm.getProducts().get(3), 1);
-		List<Entry<Product, Integer>> entries = new ArrayList<>(warenkorb.entrySet());
-		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("warenkorb", entries);
-		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("warenkorb_size", warenkorb.size());
+		warenkorb.put(pm.getProducts().get(1).getName(), 20);
+		warenkorb.put(pm.getProducts().get(2).getName(), 12);
+		warenkorb.put(pm.getProducts().get(3).getName(), 9);
 		
-		
+		warenkorbKeys = new ArrayList<String>(warenkorb.keySet());
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("warenkorb", warenkorb);
 		
 		System.out.println("LOGIN");
 		
@@ -81,8 +81,11 @@ public class UserManagedBean implements Serializable {
 	}
 	
 	
-	public String cartDeleteItem() {
-		return "";
+	public String cartDeleteItem(String key) {
+		warenkorb.remove(key);
+		warenkorbKeys = new ArrayList<String>(warenkorb.keySet());
+		
+		return "warenkorb.xhtml";
 	}
 	
 	public String logout() {
@@ -95,6 +98,11 @@ public class UserManagedBean implements Serializable {
 		//manager.addUser(user.getID(), user.getName(), user.getEmail(), user.getPasswort(), user.getSessionID());
 		System.out.println("REGISTRIEREN");
 		return "login.xhtml";
+	}
+	
+	
+	public List<String> getWarenkorbKeys(){
+	     return warenkorbKeys;
 	}
 	
 	
