88777b382a37ec58a79a1477d93a6dc630cbd5cb
Max <pallaschmaximilian@gmail.com>
2017-06-29 13:59:42 +0200

    Finished it

--- webengProjekt/src/main/java/webeng/validation/EmailValidator.java
+++ webengProjekt/src/main/java/webeng/validation/EmailValidator.java
@@ -5,14 +5,24 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.faces.application.FacesMessage;
 
 @FacesValidator("globalShop.EmailValidator")
 public class EmailValidator implements Validator{
 
 	@Override
 	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
-		// TODO Auto-generated method stub
-		
+		boolean error = true;
+		if(value instanceof String) {
+			error = false;
+			String s = (String)value;
+			if(!(s.indexOf('@')==1)) {
+				error = true;
+			}
+		}
+		if(error) {
+			throw new ValidatorException(new FacesMessage("Keine EMail-Adresse", "Der angegebene String ist keine EMail-Adresse"));
+		}
 	}
 
 }
