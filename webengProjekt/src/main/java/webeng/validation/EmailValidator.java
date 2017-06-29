e85e2d9bc4abcdd8d2b0afa83d3b8464a367bfea
Max <pallaschmaximilian@gmail.com>
2017-06-29 13:31:24 +0200

    Started to implement EmailValidator

---
+++ webengProjekt/src/main/java/webeng/validation/EmailValidator.java
@@ -0,0 +1,18 @@
+package webeng.validation;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("globalShop.EmailValidator")
+public class EmailValidator implements Validator{
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
